297a125937bdc22521b000094951c2a5c16f7dca
Mathuja991 <mathujaparameshwaran@gmail.com>
2024-10-30 09:20:48 +0530

    Delete industrial_diary_system directory

--- industrial_diary_system/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Enable error reporting (optional for debugging)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Redirect to the login page
-header('Location: public/login.php');
-exit();
-?>
